11748088ffa1e8fff2afd976bbf7af87157a2551
JASNAJazz <134054342+JASNAJazz@users.noreply.github.com>
2025-11-04 15:38:53 +0530

    Add files via upload

---
+++ prgrm2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int n,i;
+	printf("enter number of elements :");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter %d elements :\n",n);
+	for(i=0;i<n;i++){
+	scanf("%d",&arr[i]);
+	}
+	int max=arr[0];
+	for(i=0;i<n;i++){
+	if(arr[i] > max){
+	max=arr[i];		
+	}}
+	printf("maximum elements in the array =%d \n",max);
+	return 0;
+	}
+
